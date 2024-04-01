c9c1d09ef86471520d370818a4c32bcd72e10c93
OriginalAnkit <akt.halder@gmail.com>
2024-04-01 23:34:08 +0530

    working slider

--- script.js
+++ script.js
@@ -4,15 +4,14 @@
 
 
 
-// $(document).ready(function () {
 
-
-const changeTime = 4000
 const num_of_ele = 3
 let active_ele = 0
 let progress_dict = {}
 function activeProgressBar() {
     let intv = setInterval(function () {
+        $(`.card-slider .care-corner-slider`).removeClass('active')
+        $($(`.card-slider .care-corner-slider`)[active_ele]).addClass('active')
         if (progress_dict[active_ele]) {
             progress_dict[active_ele] += 2.4
         } else {
@@ -20,7 +19,7 @@ function activeProgressBar() {
         }
         $($(`.card-slider .progress .progress-bar`)[active_ele]).css("width", `${progress_dict[active_ele]}%`)
         if (progress_dict[active_ele] >= 100) {
-            clearInterval(intv)
+            // clearInterval(intv)
             progress_dict[active_ele] = 0
             $(`.card-slider .progress .progress-bar`).css("width", `${0}%`)
             active_ele++
@@ -32,16 +31,6 @@ function activeProgressBar() {
 
 
 }
-function changeElement() {
-    activeProgressBar()
-    $(`.card-slider .care-corner-slider`).removeClass('active')
-    $($(`.card-slider .care-corner-slider`)[active_ele]).addClass('active')
 
-}
+activeProgressBar()
 
-
-changeElement(active_ele)
-setInterval(() => {
-    changeElement(active_ele)
-}, changeTime);
-// });
