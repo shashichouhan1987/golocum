8be171fd86bb1cb491f4547982b327afac8b4619
OriginalAnkit <akt.halder@gmail.com>
2024-04-01 17:23:34 +0530

    logic working

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 
+
+
+
+
+
+// $(document).ready(function () {
+
+
 const changeTime = 4000
 const num_of_ele = 3
-let active_ele = 1
-function changeElement(index) {
-    $(`.card-slider .care-corner-slider`).removeClass('active')
-    $(`.card-slider .care-corner-slider:nth-child(${active_ele})`).addClass('active')
-    active_ele++
-    if (active_ele > num_of_ele) {
-        active_ele = 1
-    }
+let active_ele = 0
+let progress_dict = {}
+function activeProgressBar() {
+    let intv = setInterval(function () {
+        if (progress_dict[active_ele]) {
+            progress_dict[active_ele] += 2.4
+        } else {
+            progress_dict[active_ele] = 2.4
+        }
+        $($(`.card-slider .progress .progress-bar`)[active_ele]).css("width", `${progress_dict[active_ele]}%`)
+        if (progress_dict[active_ele] >= 100) {
+            clearInterval(intv)
+            progress_dict[active_ele] = 0
+            $(`.card-slider .progress .progress-bar`).css("width", `${0}%`)
+            active_ele++
+            if (active_ele >= num_of_ele) {
+                active_ele = 0
+            }
+        }
+    }, 90);
+
+
 }
+function changeElement() {
+    activeProgressBar()
+    $(`.card-slider .care-corner-slider`).removeClass('active')
+    $($(`.card-slider .care-corner-slider`)[active_ele]).addClass('active')
+
+}
+
 
 changeElement(active_ele)
 setInterval(() => {
     changeElement(active_ele)
 }, changeTime);
+// });
